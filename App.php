ee39f4631c5674b046e2e12e08b458603ca119fc
faizmuhammad02 <faizmuhammad02@gmail.com>
2021-06-15 15:03:44 +0700

    add ViewShowToDoList.php, ViewAddToDoList.php, ViewRemoveToDoList.php

--- App.php
+++ App.php
@@ -3,5 +3,8 @@ require_once "Model/ToDoList.php";
 require_once "BusinessLogic/ShowToDoList.php";
 require_once "BusinessLogic/AddToDoList.php";
 require_once "BusinessLogic/RemoveToDoList.php";
+require_once "View/ViewShowToDoList.php";
+require_once "View/ViewAddToDoList.php";
+require_once "View/ViewRemoveToDoList.php";
 
 echo "Aplikasi Todo List" . PHP_EOL;
