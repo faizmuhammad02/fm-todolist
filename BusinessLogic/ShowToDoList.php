dd178206b589b20b1f0d8aeae56e903ac529f023
faizmuhammad02 <faizmuhammad02@gmail.com>
2021-06-15 16:14:58 +0700

    add foreach inside function showToDoList

--- BusinessLogic/ShowToDoList.php
+++ BusinessLogic/ShowToDoList.php
@@ -3,4 +3,10 @@
 // Menampilkan to do list
 function showToDoList()
 {
+    global $todolist;
+    echo "TO DO LIST" . PHP_EOL;
+
+    foreach ($todolist as $number => $value) {
+        echo "$number. $value" . PHP_EOL;
+    }
 }
